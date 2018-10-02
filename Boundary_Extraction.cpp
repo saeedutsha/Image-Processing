cd0f79f0ef20f8c11e64b17189597262bb083f8f
Kazi Saeed Alam <sayed.utsha777@gmail.com>
2018-10-02 23:52:19 +0600

    Boundary extraction of an image using Erosion operation.

---
+++ Boundary_Extraction.cpp
@@ -0,0 +1,89 @@
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <iostream>
+#include <math.h>
+
+
+using namespace std;
+using namespace cv;
+
+Mat boundary(Mat des, int maskSize) {
+
+	int s;
+	s = maskSize / 2;
+	Mat mask;
+	mask.create(maskSize, maskSize, CV_32S);
+	Mat out,bout;
+	out.create(des.size(), des.type());
+	bout.create(des.size(), des.type());
+
+	int flag = 0;
+
+	for (int y = 0; y < des.rows - maskSize + 1; y++)
+	{
+		for (int x = 0; x < des.cols - maskSize + 1; x++)
+		{
+			flag = 0;
+
+			for (int y1 = 0; y1 < maskSize; y1++)
+			{
+				for (int x1 = 0; x1 < maskSize; x1++)
+				{
+
+					if (des.at<uchar>(y + y1, x + x1) != 255) {
+						flag = 1;
+					}
+
+				}
+			}
+			if (flag == 1)
+				out.at<uchar>(y + s, x + s) = 0;
+			else
+				out.at<uchar>(y + s, x + s) = des.at<uchar>(y + s, x + s);
+
+		}
+	}
+
+	for (int y = 0; y < des.rows; y++)
+	{
+		for (int x = 0; x < des.cols; x++)
+		{
+			bout.at<uchar>(y, x) = des.at<uchar>(y, x) - out.at<uchar>(y, x);
+		}
+	}
+
+
+	imshow("After Erosion",out);
+	waitKey();
+	return bout;
+
+}
+
+
+int main() {
+
+	Mat src = imread("sam2.tif", CV_LOAD_IMAGE_GRAYSCALE);
+	int maskSize;
+	cout << "Enter mask size : ";
+	cin >> maskSize;
+	Mat des;
+	int s;
+	s = maskSize / 2;
+	copyMakeBorder(src, des, s, s, s, s, 0, 0);
+	for (int y = 0; y < des.rows; y++)
+	{
+		for (int x = 0; x < des.cols; x++)
+		{
+			if (des.at<uchar>(y, x) >= 0 && des.at<uchar>(y, x) <= 127)des.at<uchar>(y, x) = 0;
+			else if (des.at<uchar>(y, x) >127 && des.at<uchar>(y, x) <= 255) des.at<uchar>(y, x) = 255;
+
+		}
+	}
+
+	imshow("Input", src);
+	waitKey();
+	Mat output = boundary(des, maskSize);
+	imshow("After Boundary Extraction", output);
+	waitKey();
+
+}
